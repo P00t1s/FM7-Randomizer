39d60e0ccf06d4fce15740a47793499d9ca0792f
Xavier JD <54561121+P00t1s@users.noreply.github.com>
2022-12-14 16:50:26 +1300

    Track condition selected based on track itself
    Track condition is now selected based on if the track exists in a list. This bugs out and gives a bad track name if the selecttrack string can be autocompleted by a track in the tracklist.

--- fm7random.py
+++ fm7random.py
@@ -6,11 +6,17 @@ import re
 connec = sqlite3.connect("Data.db")
 cursor = connec.cursor()
 
-# Get Table Sizes
+# Get Table Sizes and Make list variables for comparision.
 cursor.execute("SELECT * FROM Cars")
 carlistlimit = len(cursor.fetchall())
 cursor.execute("SELECT * FROM Tracks")
 tracklistlimit = len(cursor.fetchall())
+cursor.execute("SELECT * FROM Tracks")
+tracklist = re.sub(r"[(),']", "", str(cursor.fetchall()))
+cursor.execute("SELECT * FROM NightTrack")
+nightlist = re.sub(r"[(),']", "", str(cursor.fetchall()))
+cursor.execute("SELECT * FROM RainTrack")
+rainlist = re.sub(r"[(),']", "", str(cursor.fetchall()))
 cursor.execute("SELECT * FROM WeatherType")
 weatherlistlimit = len(cursor.fetchall())
 
@@ -27,28 +33,33 @@ selecttrack = re.sub(r"[(),']", "", str(cursor.fetchone()))
 
 # Set condition based on Track selection
 selectcondition = ""
-if selecttrack == "Bathurst":
-    selectcondition = "Day"
-elif selecttrack == "Brands Hatch GP" or selecttrack == "Spa Full":
-    conditionrandom = random.randrange(1, 10, 1)
-    if conditionrandom > 5:
-        selectcondition = "Day"
-    else:
-        selectcondition = "Rain"
-elif selecttrack == "Brands Hatch GP" or selecttrack == "":
-    conditionrandom = random.randrange(1, 10, 1)
-    if conditionrandom > 5:
-        selectcondition = "Day"
-    else:
-        selectcondition = "Night"
-elif selecttrack == "Homestead Full" or selecttrack == "Spa Full":
-    conditionrandom = random.randrange(9, 18, 1)
+selecttrack = "Ba"
+if selecttrack in rainlist and selecttrack in nightlist:
+    conditionrandom = random.randrange(0, 9, 1)
     if conditionrandom <= 3:
         selectcondition = "Day"
     elif conditionrandom < 6 and conditionrandom > 3:
         selectcondition = "Night"
     else:
         selectcondition = "Rain"
+elif selecttrack in rainlist:
+    conditionrandom = random.randrange(1, 10, 1)
+    if conditionrandom > 5:
+        selectcondition = "Day"
+    else:
+        selectcondition = "Rain"
+elif selecttrack in nightlist:
+    conditionrandom = random.randrange(1, 10, 1)
+    if conditionrandom > 5:
+        selectcondition = "Day"
+    else:
+        selectcondition = "Night"
+elif selecttrack in tracklist:
+    selectcondition = "Day"
+# Should catch cases of the track selection not being in the database
+else:
+    print("Not sure how this happened, but the randomly selected track doesn't exist in the database")
+    quit()
 
 # If the condition is set to Rain, select the three options used for the race and their respective probabilities
 selectweather = []
